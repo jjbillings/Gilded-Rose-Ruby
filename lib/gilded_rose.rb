62ed4786f193f8e3355a8dc46e333973ca1d4127
jjbillings <jackjbill@gmail.com>
2019-05-22 06:21:33 -0500

    Backstage and Sulfras ticks

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -24,56 +24,29 @@ class GildedRose
     @quality += 1 if @days_remaining < 0
   end
 
-  def tick
-    if name == "Normal Item"
-      return normal_tick
-    end
+  def backstage_tick
+    @days_remaining -= 1
+    return if @quality == 50
 
-    if name == "Aged Brie"
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+    @quality += 1
+    @quality = 0 if @days_remaining < 0
+  end
+
+  def sulfras_tick
+  end
+
+  def tick
+    case @name
+    when "Normal Item"
+      return normal_tick
+    when "Aged Brie"
       return aged_brie_tick
-    end
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
+    when "Backstage passes to a TAFKAL80ETC concert"
+      return backstage_tick
+    when "Sulfuras, Hand of Ragnaros"
+      return sulfras_tick
     end
   end
 end
